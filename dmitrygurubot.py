da0483470d99dec10e97c241f023ef27f39488dc
dev4ox <dev4ox@gmail.com>
2023-07-30 04:12:29 +0300

    ver. 1.0

--- dmitrygurubot.py
+++ dmitrygurubot.py
@@ -25,7 +25,7 @@ bonus_mes = '<b>Поздравляю!</b> Ты уже на шаг ближе к 
                 '<u>Доступ к нему будет закрыт 31.07.2023г в 23:59.</u>\n\n' \
                 'Если ты хочешь узнать пошаговый план по запуску успешного бизнеса, а именно: как собрать команду, где брать клиентов, как заключать договора, как масштабироваться, как внедрять систему аналитики, как привлекать инвестиции и многие другие бизнес инструменты и процессы, то прямо сейчас оплачивай доступ со скидкой 70% всего <s>3200₽</s>=990₽\n\n' \
                 '<b>ВАЖНО!</b>👇\n' \
-                'Скидка действует всего 5 минут.\n' \
+                'Скидка действует <b>15 минут</b>.\n' \
                 'Дальше доступ можно будет приобрести только за полную стоимость.\n\n' \
                 'Успей попасть в окружение миллионера по цене одной пиццы 🍕\n\n' \
                 '<i>P.s. сразу же после оплаты пришлем тебе приглашение в канал.</i>\n' \
@@ -33,14 +33,8 @@ bonus_mes = '<b>Поздравляю!</b> Ты уже на шаг ближе к 
                 'Вот кнопка на оплату, скорее оплачивай, время уже идет 👇'
 
 
-# Покупка через ЮКасса
-def buy_sub(user_id):
-    url = 't.me/gurutda'
-    return url
-
-
 # Функция для добавления пользователя в базу данных или обновления статуса подписки
-def add_or_update_user(user_id, username, first_name, last_name, subpub, subpriv, buy_sub):
+def db_query(user_id:str, username:str, first_name:str, last_name:str, subpub:bool, subpriv:bool, buy_sub:int, parametr:int):
     try:
         conn = sqlite3.connect('users.db')
         cursor = conn.cursor()
@@ -60,15 +54,22 @@ def add_or_update_user(user_id, username, first_name, last_name, subpub, subpriv
         cursor.execute("SELECT id FROM users WHERE id=?", (user_id,))
         existing_user = cursor.fetchone()
         # Если пользователь уже существует, обновляем статус подписки
-        if existing_user:
+        # Обновление статуса подписки на паблик
+        if existing_user and parametr == 1:
             cursor.execute("UPDATE users SET subpub=? WHERE id=?", (subpub, user_id))
+        # Обновление стоимости, за которую была куплена подписка
+        elif existing_user and parametr == 2:
+            cursor.execute("UPDATE users SET buy_sub=? WHERE id=?", (buy_sub, user_id))
+        # Обновление статуса подписки на приват
+        elif existing_user and parametr == 3:
+            cursor.execute("UPDATE users SET subpriv=? WHERE id=?", (subpriv, user_id))
         else:
             # Получаем текущую дату и время
             current_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
             # Добавляем пользователя в базу данных со статусом подписки
             cursor.execute('INSERT INTO users (id, username, first_name, last_name, reg_date, subpub, subpriv, buy_sub) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
                            (user_id, username, first_name, last_name, current_date, subpub, subpriv, buy_sub))
+
         conn.commit()
     except Exception as e:
         pass
@@ -76,13 +77,37 @@ def add_or_update_user(user_id, username, first_name, last_name, subpub, subpriv
         conn.close()
 
 
-# Проверка подписки
-def check_pubsub(user_id, check_channel):
+# Покупка через ЮКасса
+def buy_sub(message, count:str):
+    # Создание объекта счета на оплату
+    invoice = telebot.types.InlineKeyboardMarkup()
+    item = telebot.types.LabeledPrice(label='Оплата', amount=int(count+'00'))  # 100 копеек, то есть 1 рубль
+    # Отправка запроса на оплату
+    bot.send_invoice(message.chat.id, title='💎 Приватный доступ 💎',
+                     description='Покупка доступа к закрытому каналу\n "Делаем 10 миллионов" | Реалити (бонус)',
+                     provider_token=key.yootoken,
+                     currency='RUB', prices=[item], start_parameter='pay_001', invoice_payload=count)
+
+
+# Приглашение в приватный канал
+def user_invite_subpriv(user_id):
+    invite_priv_button = types.InlineKeyboardMarkup(row_width=1)
+    invite_priv_button.add(types.InlineKeyboardButton(text='Приватный канал', url=key.url_channel_privat))
     try:
-        chat_member = bot.get_chat_member(user_id, check_channel)
+        db_query(user_id, '', '', '', '', True, '', 3)
+        bot.send_message(user_id, 'Твой доступ в приватный канал\n👇👇👇', reply_markup=invite_priv_button)
+        return True
+    except telebot.apihelper.ApiException as e:
+        return False
+
+
+# Проверка подписки
+def check_pubsub(check_channel, user_id):
+    try:
+        chat_member = bot.get_chat_member(check_channel, user_id)
         if chat_member.status == 'member' or chat_member.status == 'administrator':
             # Обновляем статус подписки на True в базе данных
-            add_or_update_user(user_id, '', '', '', True, '', '')
+            db_query(user_id, '', '', '', True, '', '', 1)
             return True
     except telebot.apihelper.ApiException:
         pass
@@ -92,14 +117,14 @@ def check_pubsub(user_id, check_channel):
 def user_give_bonus(message):
     user_id = message.chat.id
     subpub_button = types.InlineKeyboardMarkup(row_width=1)
-    subpub_button.add(types.InlineKeyboardButton(text='Подписаться', url='https://t.me/dimonbataysk'))
+    subpub_button.add(types.InlineKeyboardButton(text='Подписаться', url=key.url_channel_public))
     buy_button = types.InlineKeyboardMarkup(row_width=1)
-    buy_button.add(types.InlineKeyboardButton(text='💎 Купить доступ', callback_data='user_buy_sub'))
-    if check_pubsub(user_id, key.id_channel_public) == False:
+    buy_button.add(types.InlineKeyboardButton(text='Забрать бонус', callback_data='user_buy_sub'))
+    if check_pubsub(key.id_channel_public, user_id) == False:
         bot.send_message(user_id, 'Сначала подпишись на канал', reply_markup=subpub_button)
     # Проверка подписки
     while True:
-        user_is_subscribed = check_pubsub(user_id, key.id_channel_public)
+        user_is_subscribed = check_pubsub(key.id_channel_public, user_id)
         if user_is_subscribed == True:
             bot.send_message(user_id, bonus_mes, parse_mode='html', reply_markup=buy_button)
             break
@@ -114,12 +139,12 @@ def admin_add_user(message):
 #Обработка команды start
 def command_start(message, user_id):
     # Запрос к БД с проверкой/добавления пользователя
-    add_or_update_user(user_id, message.from_user.username, message.from_user.first_name, message.from_user.last_name,
-                       False, False, 0)
+    db_query(user_id, message.from_user.username, message.from_user.first_name, message.from_user.last_name,
+                       False, False, 0, 1)
     # Отправка приветственного сообщения
     bonus_button = types.InlineKeyboardMarkup(row_width=1)
-    bonus_button.add(types.InlineKeyboardButton(text='💎 Забрать бонус 💎', callback_data='user_give_bonus'))
-    with open('welcome.jpeg', 'rb') as photo:
+    bonus_button.add(types.InlineKeyboardButton(text='💰 Твой бонус 💰', callback_data='user_give_bonus'))
+    with open('welcome.jpg', 'rb') as photo:
         bot.send_photo(chat_id=user_id, photo=photo, parse_mode='html', caption=welcome_mes, reply_markup=bonus_button)
 
 
@@ -145,7 +170,11 @@ def start_bot():
     @bot.callback_query_handler(func=lambda call: True)
     def callback_inline(call):
         if call.data == 'user_buy_sub':
-            buy_sub(call.message)
+            buy_sub(call.message, '100')
+            # if True:
+            #     buy_sub(call.message, '990')
+            # else:
+            #     buy_sub(call.message, '3200')
         if call.data == 'admin_view_user':
             pass
         if call.data == 'admin_add_user':
@@ -153,7 +182,21 @@ def start_bot():
         if call.data == 'user_give_bonus':
             user_give_bonus(call.message)
 
+    @bot.pre_checkout_query_handler(func=lambda query: True)
+    def checkout(query):
+        # Подтверждение оплаты
+        bot.answer_pre_checkout_query(query.id, ok=True)
 
+    @bot.message_handler(content_types=['successful_payment'])
+    def successful_payment(message):
+        sub_count = int(message.successful_payment.invoice_payload)
+        db_query(message.chat.id, '', '', '', '', '', sub_count, 2)
+        user_invite_confirm = user_invite_subpriv(message.chat.id)
+        # Обработка успешной оплаты
+        bot.send_message(key.id_channel_info, f'Пользователь: {message.from_user.first_name} {message.from_user.last_name}\n'
+                                              f'Ник: @{message.from_user.username}, id: {message.chat.id}\n'
+                                              f'Купил подписку за {sub_count} RUB\n'
+                                              f'Статус подписки: {user_invite_confirm}')
 
     bot.polling(none_stop=True)
 start_bot()
